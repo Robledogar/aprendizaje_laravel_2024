891fa55507447af2c71a6f2c873800cfcbfc7069
Robledogar <robledogar@gmail.com>
2024-04-17 20:08:10 +0200

    curso page2 completado

--- curso_laravel_minutos/page2-app/resources/views/layout.blade.php
+++ curso_laravel_minutos/page2-app/resources/views/layout.blade.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+
     <title>Laravel en minutos</title>
+    
 </head>
 <body>
-    <ul>
+    <ul class="menu">
         <li><a href="{{ route('home') }}">Home</a></li>
         <li><a href="{{ route('about') }}">Acerca de</a></li>
         <li><a href="{{ route('blog.index') }}">Blog</a></li>
         <li><a href="{{ route('contact') }}">Contactos</a></li>
     </ul>
 
-    <div>
+    <div id="app">
         @yield('content')
     </div>
 </body>
---
+++ curso_laravel_minutos/page2-app/webpack.mix.js
@@ -0,0 +1,4 @@
+const mix = require('laravel-mix');
+
+   mix.styles('resources/css/app.css', 'public/css/app.css');
+
